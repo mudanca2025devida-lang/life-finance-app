046768ee590bf0e9135f8d838a55b372b6179620
mudanca2025devida-lang <mudanca2025devida@gmail.com>
2025-12-18 19:46:34 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,7 @@
-const CACHE_NAME = 'lf-monity-ai-v2'; // MUDE SEMPRE O NÚMERO
-const urlsToCache = [
+const CACHE_VERSION = 'v5';
+const CACHE_NAME = `lf-monity-ai-${CACHE_VERSION}`;
+
+const FILES = [
   '/',
   '/index.html',
   '/manifest.json'
@@ -8,15 +10,31 @@ const urlsToCache = [
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
     )
   );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
+});
+self.addEventListener('message', event => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
 
